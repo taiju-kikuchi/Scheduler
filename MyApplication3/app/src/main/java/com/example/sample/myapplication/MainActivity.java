5853c349e876f58ea2105dd2819785b11b3c0da3
Taiju Kikuchi <taiju.kikuchi.sys@gmail.com>
2017-10-30 10:49:27 +0900

    second commit

---
+++ MyApplication3/app/src/main/java/com/example/sample/myapplication/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.sample.myapplication;
+
+import android.os.Build;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.WindowManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (Build.VERSION.SDK_INT >= 19) {
+            View decor = this.getWindow().getDecorView();
+            decor.setSystemUiVisibility(View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_FULLSCREEN | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+        } else {
+            getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        }
+        setContentView(R.layout.activity_main);
+    }
+    public void pad(View v){
+        Log.d("ボタン","おした");
+    }
+
+
+
+}
